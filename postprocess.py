656cad58b227c001b5470db97eb592fa41107201
esainane <esainane+github@gmail.com>
2020-12-17 14:42:01 +1300

    Scrape detail.html for lobbyid as fallback
    This is not reliable, as a player may have had their name changed between
    when the battle occurred and when the detail.html was scraped.
    
    This is not expected to be a common scenario, and postprocess.py will
    raise a failing exit code when met. This allows for manual editing
    of events.log to provide userid.
    
    Place your bets on how many demofiles I'll need to zgrep now!

--- postprocess.py
+++ postprocess.py
@@ -8,13 +8,17 @@ from sys import argv, stderr, exit
 filename = argv[1]
 id = int(argv[2])
 
+# Scrape detail.html
 premap = re.compile(r'a href="/Maps/Detail/')
 mapname = re.compile(r'Map: (?P<name>[^<]+)')
 timestamp = re.compile(r"a href='/replays/(?P<year>[0-9]{4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})_(?P<hours>[0-9]{2})(?P<minutes>[0-9]{2})(?P<seconds>[0-9]{2})_")
+userid = re.compile(r"href='/Users/Detail/(?P<userid>[0-9]+)'[^>]+>(?P<username>[^<]+)</a>")
 
+name_to_userid = {}
 teamid_to_player = {}
 name_to_player = {}
 
+# Scrape events.log
 playerinfo = re.compile(r'\[(?:0|1)\] (?P<name>.*), team: (?P<teamid>[0-9]+), elo:(?P<elo>[0-9]+)(?:, userid: (?P<userid>[0-9]+))?(?:, ai: (?P<ai>.*))?')
 facplop = re.compile(r'\[(?P<frame>[0-9]+)\] Event \[(?P<location>[^\]]+)\]: (?P<teamid>[0-9]+) finished unit (?P<fac>Cloakbot Factory|Shieldbot Factory|Rover Assembly|Hovercraft Platform|Gunship Plant|Airplane Plant|Spider Factory|Jumpbot Factory|Tank Foundry|Amphbot Factory|Shipyard|Strider Hub)')
 draw = re.compile(r'\[(?P<frame>[0-9]+)\] Received game_message: The game ended in a draw!')
@@ -32,6 +36,7 @@ skip = False
 def d(*args, **kwargs):
     print(file=stderr, *args, **kwargs)
 
+# Scan through detail detail.html
 with open('demos/%d/detail.html' % id, 'r') as f:
     mapflag = False
     for line in f.readlines():
@@ -49,11 +54,15 @@ with open('demos/%d/detail.html' % id, 'r') as f:
         m = timestamp.search(line)
         if m:
             started = '%s-%s-%s %s:%s:%s' % (m.group('year'), m.group('month'), m.group('day'), m.group('hours'), m.group('minutes'), m.group('seconds'))
+        m = userid.search(line)
+        if m:
+            name_to_userid[m.group('username')] = m.group('userid')
 
 mapdef = re.compile(r'\| maps/(?P<map>.*)\.html$')
 spver = re.compile(r'engine/linux64/(?P<spver>[^/]*)/')
 zkver = re.compile(r'\| games/(?P<zkver>Zero-K\\ [v0-9.]+)$')
 
+# Scan through the dependency information. This is derived from detail.html, but faster to read through here.
 with open('demos/%d/events.log.deps' % id, 'r') as f:
     for line in f.readlines():
         m = spver.search(line)
@@ -65,6 +74,7 @@ with open('demos/%d/events.log.deps' % id, 'r') as f:
             zk = m.group('zkver').replace(r'\ ', ' ')
             continue
 
+# Scan through the event log.
 with open(filename, 'r') as f:
     for line in f.readlines():
         m = playerinfo.match(line)
@@ -104,9 +114,17 @@ if skip:
     print(json.dumps({'skip': True}))
     exit(0)
 
+def ensure_supplementary(player_data):
+    if 'userid' in player_data and player_data['userid'] is not None:
+        return
+    player_data['userid'] = name_to_userid[player_data['name']]
+
+
 def player_data_by_winning(name_to_player, win):
     winning_player = name_to_player[win]
     losing_player = [p for p in name_to_player.values() if p['name'] != win][0]
+    ensure_supplementary(winning_player)
+    ensure_supplementary(losing_player)
     return winning_player, losing_player
 
 def truncate_if_numeric(v, truncate_to_bits=24):
@@ -163,6 +181,8 @@ summary['winner_fac'] = winning_player['facplop'][0] if len(winning_player['facp
 summary['winner_fac_prog'] = winning_player['facplop']
 summary['loser_fac'] = losing_player['facplop'][0] if len(losing_player['facplop']) else 'Never'
 summary['loser_fac_prog'] = losing_player['facplop']
+summary['winner_userid'] = winning_player['userid']
+summary['loser_userid'] = losing_player['userid']
 summary['duration'] = int(duration)
 summary['gameid'] = id
 summary['started'] = started
