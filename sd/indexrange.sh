8d2694d03ad35103955218e54df96e3567abc1f0
esainane <esainane+github@gmail.com>
2021-04-04 14:33:17 +1200

    Fetch 1v1 Pro Host games too

--- sd/indexrange.sh
+++ sd/indexrange.sh
@@ -2,12 +2,24 @@
 
 set -e
 
+retrieve_all_types() {
+    local offset="$1"
+    shift
+    while [ $# -gt 0 ]; do
+        local title="$1"
+        shift
+        curl -s "https://zero-k.info/Battles?Title=${title}&Map=&PlayersFrom=2&PlayersTo=2&Age=0&Mission=2&Bots=2&Rank=8&Victory=0&Offset=$offset" | sed -n "s_^.*<a href='/Battles/Detail/\([0-9]\+\)'.*\$_\1_p"
+        sleep 0.2
+    done
+}
+
+
 main() {
     local start="$1"
     local end="$2"
+    shift 2
     while read offset; do
-        curl -s "https://zero-k.info/Battles?Title=MM+&Map=&PlayersFrom=2&PlayersTo=2&Age=0&Mission=2&Bots=2&Rank=8&Victory=0&Offset=$offset" | sed -n "s_^.*<a href='/Battles/Detail/\([0-9]\+\)'.*\$_\1_p"
-        sleep 0.2
+        retrieve_all_types "$offset" "$@"
     done < <(seq "$start" 40 "$end") | grep -vxFf demos/exclude.txt | paste -d\  -s - | sed 's/^/BATTLEIDS:=/' > demos/index.mk.tmp
     mv -f demos/index.mk.tmp demos/index.mk.in
     mv -f demos/index.mk.in demos/index.mk
@@ -23,4 +35,4 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
-main "$1" "$2"
+main "$1" "$2" "MM+" "[A]%20Pro%201v1%20Host"
