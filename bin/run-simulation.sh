d15fc709673359542284d4f56fc0af6c84237c9c
esainane <esainane+github@gmail.com>
2025-11-07 03:38:42 +0000

    Fix: run-simulation.sh
    Fix bad regex, and actually match the text to look for.
    
    Use PGIDs properly, rather than trying to use job control.

--- bin/run-simulation.sh
+++ bin/run-simulation.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-set -emuo pipefail
+set -euo pipefail
 
 early_exit_watcher() {
     local id="$1"
+    local sim_pgid="$2"
 
     local seen_replay_stats_lua=""
     # Watch for conditions which the replay analysis widget cannot
     while read -r line; do
         # Have we reached the end of the replay, without reaching a normal exit?
-        if [[ "$line" =~ "End of replay reached$" ]]; then
+        if [[ "$line" =~ End\ of\ demo\ reached$ ]]; then
             echo "Watchdog: Replay EOF detected."
             sleep 15
             echo "Watchdog: 15 seconds past end of replay without normal exit. Killing simulation." >&2
-            kill %2 2>/dev/null ||:
+            printf "%d (Automatic) Non-terminating replay detected. Added %s\n" "$id" "$(date --iso-8601)" >> ../../demos/exclude.txt
+            kill -- -"$sim_pgid" 2>/dev/null ||:
+            break
         fi
         # Has the simulation started without the replay analysis widget?
         # Watch for the line that indicates the widget has been loaded.
@@ -28,15 +31,10 @@ early_exit_watcher() {
         if [[ -z "$seen_replay_stats_lua" ]] && [[ "$line" =~ ^\[t=[0-9:\.]+\]\[f=[^-] ]]; then
             echo "Watchdog: Replay simulation started without loading replay_stats.lua. Killing simulation." >&2
             printf "%d (Automatic) Could not load replay_stats.lua. Added %s\n" "$id" "$(date --iso-8601)" >> ../../demos/exclude.txt
-            kill %2 2>/dev/null ||:
+            kill -- -"$sim_pgid" 2>/dev/null ||:
+            break
         fi
     done < <(tail -F "stats/$id/spring.log")
-}
-
-run_timeout() {
-    local timeout="$1"
-    shift
-    /usr/bin/timeout -k 30s "$timeout" /usr/bin/time -v "$@"
 }
 
 run_spring() {
@@ -49,18 +47,11 @@ run_spring() {
     local spring_version=$(sed -nf ../../bin/get-spring-version.sed demos/$id/detail.html)
     local spring_bin="$zkdir/engine/linux64/${spring_version}/spring-headless"
 
-    if ! run_timeout "$timeout" "$spring_bin" -write-dir "$zkdir" -config "${work_dir}/springsettings.$id.cfg" "$replay" 2> >(
-        # Filter out some spammy errors from stderr
-        grep -avF 'lups/ParticleClasses/nanolasersnoshader.lua' >&2
-    ) > "stats/$id/spring.log"; then
-        CODE=$?
-        # If the command was killed by timeout or SIGKILL, auto-skip it
-        # Note that the watcher will kill this function, so it won't be triggered by the watcher.
-        if [ "$CODE" -eq 124 -o "$CODE" -eq 137 ]; then
-            printf "%d (Automatic) Timeout reached, replay skipped. Added %s\n" "$id" "$(date --iso-8601)" >> ../../demos/exclude.txt
-        fi
-        exit "$CODE"
-    fi
+    exec setsid /usr/bin/timeout --foreground -k 30s "$timeout" /usr/bin/time -v \
+        "$spring_bin" -write-dir "$zkdir" -config "${work_dir}/springsettings.$id.cfg" "$replay" 2> >(
+            # Filter out some spammy errors from stderr
+            grep -avF 'lups/ParticleClasses/nanolasersnoshader.lua' >&2
+        ) > "stats/$id/spring.log"
 }
 
 main() {
@@ -78,16 +69,34 @@ main() {
 
     # Clear any previous output, so the early exit watcher doesn't trigger a false positive
     rm -f "stats/$id/spring.log"
-    # Watch for abnormal conditions which require the simulation to be externally killed
-    early_exit_watcher "$id" &
     # Start the simulation
     run_spring "$id" "$replay" "$zkdir" "$timeout" "$work_dir" &
-    trap "set +e; rm -rf '${work_dir}'; kill %1 %2 2>/dev/null" EXIT
+    sim_pgid=$!
+    # Watch for abnormal conditions which require the simulation to be externally killed
+    early_exit_watcher "$id" "$sim_pgid" &
+    watcher_pid=$!
 
-    # Wait for either the watcher or the simulation to finish
-    wait -n %1 %2
+    trap "set +e; rm -rf '${work_dir}'; kill -- -'"$sim_pgid"' 2>/dev/null; kill '"$watcher_pid"' 2>/dev/null" EXIT
 
-    # Check we actually produced necessary output
+    # Wait for the simulation to finish
+    set +e
+    wait "$sim_pgid"; local sim_retcode=$?
+    set -e
+
+    # If the replay timed out, skip it
+    if [[ "$sim_retcode" -eq 124 ]]; then
+        printf "%d (Automatic) Timeout reached, replay skipped. Added %s\n" "$id" "$(date --iso-8601)" >> ../../demos/exclude.txt
+    fi
+
+    kill "$watcher_pid" 2>/dev/null ||:
+    wait "$watcher_pid" 2>/dev/null ||:
+
+    # Forward exit code failures
+    if [ "$sim_retcode" -ne 0 ]; then
+        exit "$sim_retcode"
+    fi
+
+    # Check we actually produced necessary output, then move it
     test -e "$zkdir/LuaUI/Logs/replay_stats/$id/events.log" && mv -f "$zkdir/LuaUI/Logs/replay_stats/$id/events.log" "stats/$id/events.log"
 }
 
